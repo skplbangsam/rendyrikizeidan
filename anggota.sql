efc8519e7bd87e00f603934402e4c54b8dbfbb7a
skplbangsam <62376196+skplbangsam@users.noreply.github.com>
2020-03-19 19:25:03 +0700

    Delete anggota.sql

--- anggota.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `anggota` (`no_anggota`, `alamat`, `nama`, `no_hp`, `ttl`) VALUES ('1', 'jalan sukagalih', 'Riki Renaldi', '082218711945', 'bandung, 30-08-96'), ('2', 'jalan pasteur', 'Rendy', '085864967022', 'bandung, 25-05-1998'), ('3', 'jalan sukajadi', 'Zei', '0895629242710', 'bandung, 25-06-1998');
-
